06b8d1d2db14b5b2a6ef0ed0d6576e2e454d8062
nilgunCydeo <nyucedal@gmail.com>
2024-11-11 22:17:10 +0000

    initial commit

---
+++ src/test/java/com/imdb/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.imdb.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+            features = "src/test/resources/features",
+            glue = "com/imdb/step_defs",
+            dryRun = false,
+            tags = " ",
+            publish = true// generating a report with public link
+
+)
+
+public class CukesRunner {}
---
+++ src/test/java/com/imdb/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.imdb.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/imdb/step_defs"
+)
+
+public class FailedTestRunner {
+}
